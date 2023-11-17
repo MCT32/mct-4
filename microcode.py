a3a65d9c275a82f58e379efc460013e219f8a4e4
MCT32 <32090502+MCT32@users.noreply.github.com>
2023-11-17 12:19:03 +1100

    NOP, HLT, LDI, LDA, STA

--- microcode.py
+++ microcode.py
@@ -15,70 +15,70 @@ AS =  0b0010000000000000
 OI =  0b0100000000000000
 
 code = [
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0000 nc nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0000 nc  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0000  c nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0000  c  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0001 nc nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0001 nc  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0001  c nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0001  c  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0010 nc nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0010 nc  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0010  c nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0010  c  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0011 nc nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0011 nc  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0011  c nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0011  c  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0100 nc nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0100 nc  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0100  c nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0100  c  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0101 nc nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0101 nc  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0101  c nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0101  c  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0110 nc nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0110 nc  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0110  c nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0110  c  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0111 nc nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0111 nc  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0111  c nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 0111  c  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1000 nc nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1000 nc  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1000  c nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1000  c  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1001 nc nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1001 nc  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1001  c nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1001  c  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1010 nc nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1010 nc  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1010  c nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1010  c  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1011 nc nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1011 nc  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1011  c nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1011  c  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1100 nc nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1100 nc  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1100  c nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1100  c  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1101 nc nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1101 nc  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1101  c nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1101  c  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1110 nc nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1110 nc  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1110  c nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1110  c  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1111 nc nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1111 nc  z
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1111  c nz
-    CO|MI,  RO|II|CE,   0,  0,  0, # 1111  c  z
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 0000 nc nz NOP
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 0000 nc  z
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 0000  c nz
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 0000  c  z
+    CO|MI,  RO|II|CE,   IO|AI,  0,      0,      0, 0, 0, # 0001 nc nz LDI
+    CO|MI,  RO|II|CE,   IO|AI,  0,      0,      0, 0, 0, # 0001 nc  z
+    CO|MI,  RO|II|CE,   IO|AI,  0,      0,      0, 0, 0, # 0001  c nz
+    CO|MI,  RO|II|CE,   IO|AI,  0,      0,      0, 0, 0, # 0001  c  z
+    CO|MI,  RO|II|CE,   IO|MI,  RO|AI,  0,      0, 0, 0, # 0010 nc nz LDA
+    CO|MI,  RO|II|CE,   IO|MI,  RO|AI,  0,      0, 0, 0, # 0010 nc  z
+    CO|MI,  RO|II|CE,   IO|MI,  RO|AI,  0,      0, 0, 0, # 0010  c nz
+    CO|MI,  RO|II|CE,   IO|MI,  RO|AI,  0,      0, 0, 0, # 0010  c  z
+    CO|MI,  RO|II|CE,   IO|MI,  AO|RI,  0,      0, 0, 0, # 0011 nc nz STA
+    CO|MI,  RO|II|CE,   IO|MI,  AO|RI,  0,      0, 0, 0, # 0011 nc  z
+    CO|MI,  RO|II|CE,   IO|MI,  AO|RI,  0,      0, 0, 0, # 0011  c nz
+    CO|MI,  RO|II|CE,   IO|MI,  AO|RI,  0,      0, 0, 0, # 0011  c  z
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 0100 nc nz
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 0100 nc  z
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 0100  c nz
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 0100  c  z
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 0101 nc nz
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 0101 nc  z
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 0101  c nz
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 0101  c  z
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 0110 nc nz
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 0110 nc  z
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 0110  c nz
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 0110  c  z
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 0111 nc nz
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 0111 nc  z
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 0111  c nz
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 0111  c  z
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1000 nc nz
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1000 nc  z
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1000  c nz
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1000  c  z
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1001 nc nz
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1001 nc  z
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1001  c nz
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1001  c  z
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1010 nc nz
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1010 nc  z
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1010  c nz
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1010  c  z
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1011 nc nz
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1011 nc  z
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1011  c nz
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1011  c  z
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1100 nc nz
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1100 nc  z
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1100  c nz
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1100  c  z
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1101 nc nz
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1101 nc  z
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1101  c nz
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1101  c  z
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1110 nc nz
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1110 nc  z
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1110  c nz
+    CO|MI,  RO|II|CE,   0,      0,      0,      0, 0, 0, # 1110  c  z
+    CO|MI,  RO|II|CE,   HLT,    0,      0,      0, 0, 0, # 1111 nc nz HLT
+    CO|MI,  RO|II|CE,   HLT,    0,      0,      0, 0, 0, # 1111 nc  z
+    CO|MI,  RO|II|CE,   HLT,    0,      0,      0, 0, 0, # 1111  c nz
+    CO|MI,  RO|II|CE,   HLT,    0,      0,      0, 0, 0, # 1111  c  z
 ]
 
 file = open('microcode.bin', 'wb')
